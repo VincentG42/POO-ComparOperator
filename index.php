90d6315b2f00dd650df226b27c3c948eff8fc5d8
Tkumbasar <turan-kumbasar@live.fr>
2024-02-14 17:36:47 +0100

    cards

--- index.php
+++ index.php
@@ -6,8 +6,64 @@
     <link rel="stylesheet" href="./css/output.css">
     <title>Document</title>
 </head>
-<body>
-    
+
+<body class="">
+    <header class="bg-slate-400 h-32">
+        <nav>
+
+        </nav>
+        <section class="bg-slate-100 h-24">
+
+        </section>   
+    </header>
+
+
+    <section class="grid grid-rows-4 grid-flow-col gap-4 flex justify-center align-center">
+
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+    <div class="p-8 h-64 w-64 bg-blue">fsdfsfd</div>
+
+
+    </section>
+
+
+  <section class="bg-white flex justify-center align-center">
+    <div class="container mx-auto">
+      <div class="max-w-md mx-auto bg-white rounded-md shadow-md overflow-hidden">
+        <div class="flex justify-between">
+        <span class="text-lg font-medium text-gray-600">Nom :  Tour opérateur !</span>
+        <span class="text-lg font-medium text-gray-600">Note 0 / 5</span>
+        </div>
+
+        <img src="./img/Barcelone.jpg" alt="Placeholder Image" class="w-full h-56 object-cover">
+        <div class="p-4">
+          <h2 class="text-xl font-semibold text-gray-800"> <?php   ?></h2>
+          <p class="mt-2 text-gray-600">This is a simple card example using Tailwind CSS.</p>
+          <div class="mt-4 flex justify-between items-center">
+            <span class="text-sm font-medium text-gray-600">Nom</span>
+            <span class="text-sm font-medium text-gray-600">Prix</span>
+            
+          </div>
+          <div class="mt-4 flex justify-between items-center">
+            <textarea name="" id="" cols="5" rows="2"></textarea>
+            <button>Envoyer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
 
 </body>
+
+
 </html>
